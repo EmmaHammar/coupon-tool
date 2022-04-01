3a3644f845759ebc6e17d336ea3e5fac58fd5777
EmmaHammar <emma.s.hammar@gmail.com>
2022-04-01 15:55:57 +0200

    add local file picker logo tinyMCE

--- src/components/TinyEditor.js
+++ src/components/TinyEditor.js
@@ -75,8 +75,65 @@ const onEditorChange = () => {
 
                 content_style: 'body { font-family:Helvetica neue,sans-serif; font-size:14px; cursor:pointer;}img {max-width: 100px;}', //TODO change style 
                 // content_css: '../tinyEditor.css',
+               
+               //======== START of File picker for local files ======== 
+               // code from tinyMCE docs: https://www.tiny.cloud/docs/demo/file-picker/ but adjusted to React
+              /* enable title field in the Image dialog*/
+                image_title: true,
+              /* enable automatic uploads of images represented by blob or data URIs*/
+                automatic_uploads: true,
+              /*
+                URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+                images_upload_url: 'postAcceptor.php',
+                here we add custom filepicker only to Image dialog
+              */
+                file_picker_types: 'image',
+              /* and here's our custom image picker*/
+              
+              file_picker_callback: function (cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+
+                /*
+                  Note: In modern browsers input[type="file"] is functional without
+                  even adding it to the DOM, but that might not be the case in some older
+                  or quirky browsers like IE, so you might want to add it to the DOM
+                  just in case, and visually hide it. And do not forget do remove it
+                  once you do not need it anymore.
+                */
+
+                input.onchange = function () {
+                  console.log("onchange filepicker, syns när fil är vald + uppladdad"); //TODO Filen syns ej i "källa fönstret" 
+                  var file = this.files[0];
+                  console.log("file", file);
+
+                  var reader = new FileReader();
+                  reader.onload = function () {
+                    console.log("reader.onload");
+                    /*
+                      Note: Now we need to register the blob in TinyMCEs image blob
+                      registry. In the next release this part hopefully won't be
+                      necessary, as we are looking to handle it internally.
+                    */
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  window.tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    /* call the callback and populate the Title field with the file name */
+                    cb(blobInfo.blobUri(), { title: file.name });
+                  };
+                  reader.readAsDataURL(file);
+                };
+
+                input.click(); //Opens file
+              },
+               //======== END of File picker for local files ======== 
               }}
               onEditorChange={onEditorChange}
+              
             />
             : ''}    
         </div>
@@ -86,6 +143,11 @@ const onEditorChange = () => {
 };
 
 //TODO 
+// https://www.tiny.cloud/docs/demo/file-picker/
+//Fix code above for local file uploader
+
+
+
 //https://www.tiny.cloud/docs-3x/customization/TinyMCE3x@Creating_a_skin/ 
 //add proper tags depending img or text
 //choose color picker for bg, not file upload
