40732430958660b89796723c5265cb74c341c9ca
EmmaHammar <emma.s.hammar@gmail.com>
2022-04-05 23:13:03 +0200

    adding currentStep style and clickableStep and link - error about unmounted comp not solved

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,55 +3,16 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AccountContext } from '../App';
 import GetCoupon from '../services/GetCoupon';
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function Header(props) {
-  const [showCouponHeader, setShowCouponHeader] = useState(false);
+  const navigate = useNavigate(); 
   const account = useContext(AccountContext);
+  const [showCouponHeader, setShowCouponHeader] = useState(false);
 
-  // const [addNavClass, setAddNavClass] = useState(false);
-  // const couponHeaderTemplate = `
-  //   <div className='lg:flex justify-start mt-4'>
-  //     <h2 className='lg:w-2/5'>Glasskupong mars 2022</h2>
-  //     <nav className='justify-between items-center lg:w-3/5 flex flex-wrap bg-blue text-mint md:px-2'>
-  //         <Link to='/steg1'>
-  //           <h3 id='nav-logo' className='nav-menu-item'>Logga</h3>
-  //         </Link>
-  //         <Link to='/steg2'>
-  //           <h3 id='nav-background' 
-  //           className='nav-menu-item'>Bakgrund</h3>
-  //         </Link>
-  //         <Link to='/steg3'>
-  //           <h3 id='nav-text' className='nav-menu-item'>Din hälsning</h3>
-  //         </Link>
-  //         <Link to='/steg4'>
-  //           <h3 id='nav-product' className='nav-menu-item'>Välj produkt</h3>
-  //         </Link>
-  //         <Link to='/steg5'>
-  //           <h3 id='nav-summary' className='nav-menu-item'>Summering</h3>
-  //         </Link>
-  //     </nav>
-  //   </div>
-  // `;
-
-  //make steps in Stepper clickable only if currentstep is accurated filled in TODO not working
-  // useEffect( () => {
-  //   if ( props.content !== '') {
-  //     console.log("HEADER: spara i db -> nästa steg + prevSteg är clickable");
-  //   } else {
-  //     console.log("HEADER: endast prevSteg är clickable");
-
-  //   }
-
-  //   if (props.initialContent !== '') {
-  //     console.log("HEADER: spara i db -> nästa steg + prevSteg är clickable");
-  //   } else {
-  //     console.log("HEADER: endast prevSteg är clickable");
-
-  //   }
-  // })
-
-  //Show/hide couponHeader if userpage or not: TODO works correct but is not used. 
+  //TODO not finished
   useEffect( () => {
       if (props.currentStep === 'userpage') {
         // console.log("göm nav");
@@ -63,10 +24,8 @@ export default function Header(props) {
       }
   }, [props.currentStep]); 
 
-
   useEffect( () => {
     //fetch db
-
     let info = 
     {
       'pickedCouponId': account.pickedCouponId, 
@@ -78,7 +37,7 @@ export default function Header(props) {
     let navProduct = document.getElementById('nav-product');
     let navSummary = document.getElementById('nav-summary');
 
-
+    //style currentStep 
     switch (props.currentStep) {
       case 'logo': 
         navLogo.classList.add('current-step');
@@ -117,75 +76,53 @@ export default function Header(props) {
       break;
       default:
         break;
-    }
+    };
 
+    //add clickable style to step if it has data or if prevStep has data
     const cb = (res) => {
-      console.log("res Coupon Header.js:", res.coupon[0]);
-
+      // console.log("res Coupon Header.js:", res.coupon[0]);
       if (res.coupon[0].logo !== '') {
-        console.log("navLogo", navLogo);
-        navLogo.classList.add('step-done');
-        navBackground.classList.add('step-done');
+        navLogo.classList.add('clickable-step');
+        navBackground.classList.add('clickable-step');
       }
-      if ( (res.coupon[0].background === '') && (res.coupon[0].text === '') && (res.coupon[0].prodId === '')) {
-        navLogo.classList.add('step-done');
-      }
-
       if (res.coupon[0].background !== '') {
-        console.log("navBackground", navBackground);
-        navBackground.classList.add('step-done');
+        navText.classList.add('clickable-step');
       }
-      if ( (res.coupon[0].background === '') && (res.coupon[0].logo !== '') ) {
-        navBackground.classList.add('link-hover');
-      }
-      
       if (res.coupon[0].text !== '') {
-        console.log("navText", navText);
-        navText.classList.add('step-done');
+        navProduct.classList.add('clickable-step');
       }
-
-      //this don't
-      if ( (res.coupon[0].text === '') && (res.coupon[0].background !== '') ) {
-        navText.classList.add('step-done');
-      }
-
-
       if (res.coupon[0].prodId !== '') {
-        console.log("navProduct", navProduct);
-        navProduct.classList.add('step-done');
-
-        let navSummary = document.getElementById('nav-summary');
-        navSummary.classList.add('step-done');
-        console.log("navText.classList.contains('step-done'", navText.classList.contains('step-done'));
+        navSummary.classList.add('clickable-step');
       }
-      // if ( (res.coupon[0].prodId === '') && (res.coupon[0].text !== '') ) {
-      //   navProduct.classList.add('link-hover');
-      // }
     };
 
     GetCoupon(cb, info); //get data from db
   }, [props.currentStep]);
 
-  //TODO fix or remove
+
+  // add link if menu item is clickable
   const clickStep = (evt) => {
-    console.log("evt.target", evt.target.id);
-
-    switch (props.currentStep) {
-      case 'logo':
-
-        break;
-      case 'background':
-      
-        break;
-      default:
-        break;
-    }
-    
-    if (props.initialContent !== '') {
-      console.log("du kan gå till:", props.linkPath);
-    } else {
-      console.log("error, du måste fylla i något för att kunna gå till nästa steg");
-    }
+    if (document.getElementById(evt.target.id).classList.contains('clickable-step')) {
+      switch (evt.target.id ) {
+        case 'nav-logo':
+          navigate('/steg1');
+          break;
+        case 'nav-background':
+          navigate('/steg2');
+          break;
+        case 'nav-text':
+          navigate('/steg3');
+          break;
+        case 'nav-product':
+          navigate('/steg4');
+          break;
+        case 'nav-summary':
+          navigate('/steg5');
+          break;
+        default:
+          break;
+      };
+    };
   };
   
   return (
@@ -199,7 +136,7 @@ export default function Header(props) {
 
           <div className='lg:flex justify-start mt-4'>
               <h2 className='lg:w-2/5'>Glasskupong mars 2022</h2>
-              <nav className='justify-between items-center lg:w-3/5 flex flex-wrap bg-blue text-mint md:px-2 cursor-not-allowed'>
+              <nav className='justify-between items-center lg:w-3/5 flex flex-wrap bg-blue text-mint md:px-2'>
                 {/* <ul > */}
                   <h3 id='nav-logo' className='nav-menu-item logo' onClick={clickStep}>Logga</h3>
                   <h3 id='nav-background' className='nav-menu-item' onClick={clickStep}>Bakgrund</h3>
@@ -208,30 +145,13 @@ export default function Header(props) {
                   <h3 id='nav-summary' className='nav-menu-item' onClick={clickStep}>Summering</h3>
                 {/* </ul>   */}
               </nav>
-          </div>
-
-          {/* <div className='lg:flex justify-start mt-4'>
-              <h2 className='lg:w-2/5'>Glasskupong mars 2022</h2>
-              <nav className='justify-between items-center lg:w-3/5 flex flex-wrap bg-blue text-mint md:px-2'>
-                  <Link to='/steg1'>
-                    <h3 id='nav-logo' className='nav-menu-item'>Logga</h3>
-                  </Link>
-                  <Link to='/steg2'>
-                    <h3 id='nav-background' 
-                    className='nav-menu-item'>Bakgrund</h3>
-                  </Link>
-                  <Link to='/steg3'>
-                    <h3 id='nav-text' className='nav-menu-item'>Din hälsning</h3>
-                  </Link>
-                  <Link to='/steg4'>
-                    <h3 id='nav-product' className='nav-menu-item'>Välj produkt</h3>
-                  </Link>
-                  <Link to='/steg5'>
-                    <h3 id='nav-summary' className='nav-menu-item'>Summering</h3>
-                  </Link>
-              </nav>
-          </div> */}
-        
+          </div>        
     </header>
   )
 };
+
+//TODO fix error:
+// react_devtools_backend.js:3973 Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+//     at TinyEditor (http://localhost:3000/%E2%80%9D./%E2%80%9D/static/js/bundle.js:2193:66)
+//     at div
+//     at StepText (http://localhost:3000/%E2%80%9D./%E2%80%9D/static/js/bundle.js:2052:68)
